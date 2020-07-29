d53a26f2ad0d0f6774f2ab0f5848ef382635580c
Michael McVeigh <okputadora@gmail.com>
2020-07-28 17:15:26 -0700

    basic car controller - transforms not updating

--- CarController/Assets/Scripts/CarController.cs
+++ CarController/Assets/Scripts/CarController.cs
@@ -1,10 +1,57 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
     // Start is called before the first frame update
+    private float horizontalInput;
+    private float verticalInput;
+    private float steeringAngle;
+
+    public WheelCollider frontDriverWheel, frontPassengerWheel;
+    public WheelCollider rearDriverWheel, rearPassengerWheel;
+    public Transform frontDriverT, frontPassengerT;
+    public Transform rearDriverT, rearPassengerT;
+
+    public float maxSteeringAngle = 50;
+    public float motorForce = 50;
+
+    public void GetInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+
+    private void Steer()
+    {
+        steeringAngle = maxSteeringAngle * horizontalInput;
+        frontDriverWheel.steerAngle = steeringAngle;
+        frontPassengerWheel.steerAngle = steeringAngle;
+    }
+
+    private void Accelerate()
+    {
+        frontDriverWheel.motorTorque = verticalInput * motorForce;
+        frontPassengerWheel.motorTorque = verticalInput * motorForce;
+    }
+
+    private void UpdateWheelPoses()
+    {
+        UpdateWheelPose(frontPassengerWheel, frontPassengerT);
+        UpdateWheelPose(frontDriverWheel, frontDriverT);
+        UpdateWheelPose(rearPassengerWheel, rearPassengerT);
+        UpdateWheelPose(rearDriverWheel, rearDriverT);
+    }
+    private void UpdateWheelPose(WheelCollider _collider, Transform _transform)
+    {
+        Vector3 pos = _transform.position;
+        Quaternion quat = _transform.rotation;
+
+        _collider.GetWorldPose(out pos, out quat);
+    }
+
     void Start()
     {
         
@@ -14,5 +61,13 @@ public class CarController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        GetInput();
+        Steer();
+        Accelerate();
+        UpdateWheelPoses();
     }
 }
